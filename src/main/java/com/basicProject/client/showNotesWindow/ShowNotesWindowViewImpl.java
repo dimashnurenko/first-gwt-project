612d68cf91e31b18f28bbb9e1d52ec7771481840
dmitryshnurenko <dimashnurenko@mail.ru>
2014-06-12 18:05:00 +0300

    add notes

---
+++ src/main/java/com/basicProject/client/showNotesWindow/ShowNotesWindowViewImpl.java
@@ -0,0 +1,108 @@
+/*
+ * CODENVY CONFIDENTIAL
+ * __________________
+ * 
+ * [2012] - [2014] Codenvy, S.A. 
+ * All Rights Reserved.
+ * 
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
+ */
+package com.basicProject.client.showNotesWindow;
+
+import com.basicProject.client.Localization;
+import com.basicProject.client.entity.Employee;
+import com.basicProject.client.entity.Note;
+import com.basicProject.client.mvp.CallBackForNote;
+import com.basicProject.client.noteDialogWindow.NoteDialogWindowView;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.TextColumn;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.inject.Inject;
+
+import java.util.List;
+
+/**
+ * @author Dmitry Shnurenko
+ */
+public class ShowNotesWindowViewImpl extends DialogBox implements ShowNotesWindowView{
+
+    interface ShowNotesWindowUiBinder extends UiBinder<Widget, ShowNotesWindowViewImpl> {}
+
+    @UiField(provided = true)
+    CellTable tableOfNotes;
+    @UiField
+    Button    cancel;
+
+    private ActionDelegate delegate;
+
+    @Inject
+    public ShowNotesWindowViewImpl(ShowNotesWindowUiBinder ourUiBinder, Localization localization) {
+        this.tableOfNotes = createTable(localization);
+        add(ourUiBinder.createAndBindUi(this));
+
+    }
+
+    public CellTable<Note> createTable(Localization localization) {
+        CellTable<Note> table = new CellTable<>();
+
+        TextColumn<Note> title = new TextColumn<Note>() {
+            @Override
+            public String getValue(Note object) {
+                return object.getTitle();
+            }
+        };
+
+        TextColumn<Note> text = new TextColumn<Note>() {
+            @Override
+            public String getValue(Note object) {
+                return object.getText();
+            }
+        };
+
+        table.addColumn(title, localization.titleNote());
+        table.addColumn(text, localization.textNote());
+        return table;
+    }
+
+    @UiHandler("cancel")
+    public void onCancelButtonClicked(ClickEvent event){
+        hide();
+    }
+
+    @Override
+    public void setDelegate(ActionDelegate delegate) {
+        this.delegate = delegate;
+    }
+
+    public void hideWindow() {
+        hide();
+    }
+
+    @Override
+    public void showWindow() {
+        show();
+    }
+
+    public void showWindow(CallBackForNote callBack, Employee employee) {
+        show();
+    }
+
+    @Override
+    public void setNoteList(List<Note> list) {
+        tableOfNotes.setRowData(list);
+    }
+}
